3c40e81bc97b7a078ed8db66de76b0864f8a24cc
rebeccaquey <rebecca_quey@yahoo.com>
2021-04-27 17:42:55 -0700

    create seed.js to generate data for 100 cards to data.csv

---
+++ Breinify/my-app/database/seed.js
@@ -0,0 +1,23 @@
+const faker = require('faker');
+const ObjectsToCsv = require('objects-to-csv');
+
+// need to generate 100 different cards -- 
+
+let generateCard = (num) => {
+  const data = [];
+  for (let i = 0; i < num; i++) {
+    const card = {
+      id: i + 1,
+      name: faker.animal.dog(),
+      description: faker.lorem.sentences(),
+      creationTime: faker.date.between('2020-01-01', '2021-04-27')
+    }
+    data.push(card);
+  }
+  return data;
+}
+
+const allData = generateCard(100);
+
+
+new ObjectsToCsv(allData).toDisk('./data.csv');
